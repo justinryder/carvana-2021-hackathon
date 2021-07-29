2121dc692ef7981660e35846609ae0a2bc5b078e
Cesar Avitia <cesar.avitia@carvana.com>
2021-07-28 19:25:51 -0700

    use upgrade list in app

--- src/App.tsx
+++ src/App.tsx
@@ -1,39 +1,29 @@
 import React from "react";
+import { Provider } from "react-redux";
 import "./App.css";
-import { Stage, Layer, Rect, Circle } from "react-konva";
-import { Upgrade } from "./upgrade/Upgrade";
-import { Upgrade as UpgradeType } from "./upgrade/types";
-
-const upgrade: UpgradeType = {
-  id: "open_envelope",
-  name: "Open Envelope",
-  description:
-    "An advanced robot that we sourced from the future, capable or opening envelopes at lightning speed.",
-  cost: 3.5,
-  isPurchased: false,
-  apply: () => {},
-};
+import { store } from "./app/store";
+import { Stage, Layer, Text } from "react-konva";
+import { UpgradeList } from "./upgrade/UpgradeList";
 
 function App() {
   return (
     // Stage - is a div wrapper
     // Layer - is an actual 2d canvas element, so you can have several layers inside the stage
     // Rect and Circle are not DOM elements. They are 2d shapes on canvas
-    <Stage width={window.innerWidth} height={window.innerHeight}>
-      <Layer>
-        <Upgrade
-          upgrade={upgrade}
-          x={100}
-          y={100}
-          onPurchase={() => console.log("purchased")}
-          onRefund={() => console.log("refunded")}
-        />
-      </Layer>
-      {/* <Layer>
+    <Provider store={store}>
+      <Stage width={window.innerWidth} height={window.innerHeight}>
+        <Layer>
+          <Text text="Store List" />
+          <UpgradeList x={100} y={100} />
+          <Text text="Upgrade List" />
+          {/* <UpgradeList x={200} y={200} upgradeBucket="upgrades" /> */}
+        </Layer>
+        {/* <Layer>
         <Rect x={100} width={50} height={50} fill="red" onClick={() => alert('clicked rect 2')} />
         <Circle x={400} y={200} stroke="black" radius={50} onClick={() => alert('clicked circle 2')} />
       </Layer> */}
-    </Stage>
+      </Stage>
+    </Provider>
   );
 }
 
--- src/index.tsx
+++ src/index.tsx
@@ -1,16 +1,12 @@
 import React from "react";
-import { Provider } from "react-redux";
 import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
-import { store } from "./app/store";
 
 ReactDOM.render(
   <React.StrictMode>
-    <Provider store={store}>
-      <App />
-    </Provider>
+    <App />
   </React.StrictMode>,
   document.getElementById("root")
 );
