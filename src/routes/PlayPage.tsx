bab957814917603690e7761d1e78eb9a51119376
Justin Ryder <justin.ryder@carvana.com>
2021-07-30 10:25:36 -0700

    background for play page

--- src/routes/PlayPage.tsx
+++ src/routes/PlayPage.tsx
@@ -1,17 +1,20 @@
 import { Game } from "../Game";
 import { Provider } from "react-redux";
 import { store } from "../app/store";
-import { Layer, Rect } from "react-konva";
+import {Image, Layer, Rect} from "react-konva";
 import React from "react";
 import { CarmaTheme } from "../theme/CarmaTheme";
 import { useWindowBounds } from "../useWindowSize";
+import useImage from "use-image";
 
 export const PlayPage = () => {
+  const [image] = useImage('/hero_image_blue_trees.png');
   const windowBounds = useWindowBounds();
   return (
     <Provider store={store}>
       <Layer>
         <Rect {...windowBounds} fill={CarmaTheme.color.background} />
+        <Image {...windowBounds} image={image} />
         <Game />
       </Layer>
     </Provider>
